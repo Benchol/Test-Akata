a3b3e51973fb6b002c4eeb93f949b92c8de3ca09
Riana Ben Andriarinaivo <rianaben002@gmail.com>
2023-04-29 09:36:22 +0300

    Probleme 2 ok

---
+++ Probleme-2.js
@@ -0,0 +1,8 @@
+let findMaxOrMinExperiences = (experiences, isMostExperienced) => {
+    if (isMostExperienced) {
+        return experiences.map((a, i) => [i, a]).sort((a, b) => a[1] - b[1]).slice(0, 2).map(value => value[0])
+    } else {
+        return experiences.map((a, i) => [i, a]).sort((a, b) => b[1] - a[1]).slice(0, 2).map(value => value[0])
+    }
+}
+console.log(findMaxOrMinExperiences([1, 4, 3, 2, 8, 3], true))
